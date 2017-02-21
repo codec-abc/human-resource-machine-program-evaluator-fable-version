06137e368980cfb0f1c0c69deb13c8611712b1d0
cviot <cviot@optis-world.com>
2017-02-21 13:51:59 +0100

    Switch to elm like architecture

--- main.fs
+++ main.fs
@@ -2,75 +2,169 @@ namespace Main
 
 open System.IO
 open Hmrp
+
 open Fable.Core 
 open Fable.Import
 open Fable.Core.JsInterop
 
+open Fable.Arch
+open Fable.Arch.App
+open Fable.Arch.Html
+
 module Main =
 
-    let lines = 
-        [|"-- HUMAN RESOURCE MACHINE PROGRAM --";
-        "   a:";
-        "    b:";
-        "        INBOX   ";
-        "        COPYTO   0";
-        "        OUTBOX  ";
-        "        COPYFROM 0";
-        "        JUMPN    d";
-        "    c:";
-        "        JUMPZ    a";
-        "        BUMPDN   0";
-        "        OUTBOX  ";
-        "        COPYFROM 0";
-        "        JUMP     c";
-        "    d:";
-        "    e:";
-        "        BUMPUP   0";
-        "        OUTBOX  ";
-        "        COPYFROM 0";
-        "        JUMPZ    b";
-        "        JUMP     e"|]
+  let lines = 
+    [|"-- HUMAN RESOURCE MACHINE PROGRAM --";
+    "   a:";
+    "  b:";
+    "    INBOX   ";
+    "    COPYTO   0";
+    "    OUTBOX  ";
+    "    COPYFROM 0";
+    "    JUMPN  d";
+    "  c:";
+    "    JUMPZ  a";
+    "    BUMPDN   0";
+    "    OUTBOX  ";
+    "    COPYFROM 0";
+    "    JUMP   c";
+    "  d:";
+    "  e:";
+    "    BUMPUP   0";
+    "    OUTBOX  ";
+    "    COPYFROM 0";
+    "    JUMPZ  b";
+    "    JUMP   e"|]
 
-    let runTestProgram = 
-        printfn "Start"
-        let programLines = HmrpEvaluator.stringArrayToProgramList lines
-        //printProgramLines programLines
-        let initialMachineState = 
-            {
-                HmrpEvaluator.defaultMachineState with
-                    ProgramLines = programLines
-                    Inputs = [1;3;4;-2]
-                    Registers = HmrpEvaluator.buildEmptyRegisters 6
-            }
-        
-        let (states, stopReason) = HmrpEvaluator.run initialMachineState
-        try 
-            let lastState = List.last states
-            printfn "last state is:"
-            HmrpEvaluator.printState lastState
-        with 
-            | _ -> ()
+  let runTestProgram () = 
+    printfn "Start"
+    let programLines = HmrpEvaluator.stringArrayToProgramList lines
+    //printProgramLines programLines
+    let initialMachineState = 
+      {
+        HmrpEvaluator.defaultMachineState with
+          ProgramLines = programLines
+          Inputs = [1;3;4;-2]
+          Registers = HmrpEvaluator.buildEmptyRegisters 6
+      }
+    
+    let (states, stopReason) = HmrpEvaluator.run initialMachineState
+    try 
+      let lastState = List.last states
+      printfn "last state is:"
+      HmrpEvaluator.printState lastState
+    with 
+      | _ -> ()
 
-        printfn "Program has stopped to run because:\n%s" stopReason
-        //printStates states
-        let returnCode = 0 in returnCode
+    printfn "Program has stopped to run because:\n%s" stopReason
+    //printStates states
+    let returnCode = 0 in returnCode
 
+  type Model = string
 
+  type Actions =
+    | ChangeInput of string
 
-    [<EntryPoint>]
-    let main argv =
-        printfn "Start"
+  // Update
+  let update model msg =
+    printfn "%s" (msg.ToString())
+    match msg with
+      //| ChangeInput str -> str
+      | _ -> model
 
-        let addInputButton = Browser.window.document.getElementById("AddInput")
-        let onAddInputButtonClicked = (fun e -> Browser.window.alert "onAddButtonClicked")
-        addInputButton.addEventListener("click", unbox(onAddInputButtonClicked)) |> ignore
+  // View
+  //let inline onInput x = onEvent "oninput" (fun e -> x (unbox e?target?value)) 
+  let view model =
+    div
+      [ classy "ui two column stackable grid" ]
+      [
+        div
+          [ classy "three wide column"]
+          [
+            div
+              [ classy "ui segment" ]
+              [
+                text "Registers:"
+                button
+                  [ 
+                    classy "ui button"
+                    onMouseClick (fun e -> unbox(Browser.window.alert("TODO")))
+                  ]
+                  [
+                    i
+                      [
+                        classy "fa fa-plus"
+                        attribute "aria-hidden" "true"
+                      ]
+                      []
+                  ]
+              ]
+              
+          ]
+        div
+          [ classy "three wide column" ]
+          [
+            div
+              [
+                classy "ui segment"
+              ]
+              [
+                text "Inputs:"
+                button
+                  [ 
+                    classy "ui button"
+                    onMouseClick (fun e -> unbox(Browser.window.alert("TODO")))
+                  ]
+                  [
+                    i
+                      [
+                        classy "fa fa-plus"
+                        attribute "aria-hidden" "true"
+                      ]
+                      []
+                  ]
+              ]
+          ]
+        div
+          [ classy "seven wide column" ]
+          [
+            div
+              [classy "ui segment"]
+              [
+                text "Code"
+                br []
+                div
+                  [ attribute "id" "ide"]
+                  []
+              ]
+          ]
+        div
+          [classy "three wide column"]
+          [
+            div
+              [classy "ui segment"]
+              [
+                text "Run & Output"
+                br []
+                button
+                  [classy "ui button"]
+                  [
+                    i
+                      [
+                        classy "fa fa-play"
+                        attribute "aria-hidden" "true"
+                      ]
+                      []
+                  ]
+              ]
+          ]
+      ]
 
-        let addRegisterButton = Browser.window.document.getElementById("AddRegister")
-        let onAddRegisterButtonClicked = (fun e -> Browser.window.alert "onAddRegisterClicked")
-        addRegisterButton.addEventListener("click", unbox(onAddRegisterButtonClicked)) |> ignore
+  [<EntryPoint>]
+  let main argv =
 
-        let runButton = Browser.window.document.getElementById("RunButton")
-        let onRunButtonClicked = (fun e -> Browser.window.alert "onRunButtonClicked")
-        runButton.addEventListener("click", unbox(onRunButtonClicked)) |> ignore
-
-        let returnCode = 0 in returnCode
+    createSimpleApp "" view update Virtualdom.createRender
+      |> withStartNodeSelector "#app"
+      |> start
+    
+    let returnCode = 0 in returnCode
