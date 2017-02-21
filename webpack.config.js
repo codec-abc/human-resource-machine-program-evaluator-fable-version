31917ae4251868dcd39e0f702ac3f07f777be4d9
cviot <cviot@optis-world.com>
2017-02-21 17:31:07 +0100

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 
 var cfg = {
   devtool: "source-map",
-  entry: "./output/main.js",
+  entry: "./output/src/main.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
