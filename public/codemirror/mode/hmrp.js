ddb6fab2850a79d51db6e3debb6d92508223fae0
codec-abc <noemail>
2017-02-22 23:30:52 +0100

    better syntaxic coloration

--- public/codemirror/mode/hmrp.js
+++ public/codemirror/mode/hmrp.js
@@ -3,8 +3,32 @@
 
 CodeMirror.defineMode("hmrp", function () 
 {
-    var LabelRegex = "^(\\w+):$";
-    var InstructionRegex = "^(\\s+)(\\w+)(\\s*)(\\w*)$";
+    var LabelPattern = "^(\\w+):$";
+
+    var InboxPattern  = "^(\\s+)INBOX(\\s*)(\\w*)$";
+    var OutboxPattern = "^(\\s+)OUTBOX(\\s*)(\\w*)$";
+    var JumpZeroPattern = "^(\\s+)JUMPZ(\\s*)(\\w*)$";
+    var JumpNonZeroPattern = "^(\\s+)JUMPN(\\s*)(\\w*)$";
+    var JumpPattern = "^(\\s+)JUMP(\\s*)(\\w*)$";
+    var CopyToPattern = "^(\\s+)COPYTO(\\s*)(\\w*)$";
+    var CopyFromPattern = "^(\\s+)COPYFROM(\\s*)(\\w*)$";
+    var BumpUpPattern = "^(\\s+)BUMPUP(\\s*)(\\w*)$";
+    var BumpDownPattern = "^(\\s+)BUMPDN(\\s*)(\\w*)$";
+    var AddPattern = "^(\\s+)ADD(\\s*)(\\w*)$";
+    var SubPattern = "^(\\s+)SUB(\\s*)(\\w*)$";
+
+    var InboxRegex  = new RegExp(InboxPattern, "i");
+    var OutboxRegex = new RegExp(OutboxPattern, "i");
+    var JumpZeroRegex = new RegExp(JumpZeroPattern, "i");
+    var JumpNonZeroRegex = new RegExp(JumpNonZeroPattern, "i");
+    var JumpRegex = new RegExp(JumpPattern, "i");
+    var CopyToRegex = new RegExp(CopyToPattern, "i");
+    var CopyFromRegex = new RegExp(CopyFromPattern, "i");
+    var BumpUpRegex = new RegExp(BumpUpPattern, "i");
+    var BumpDownRegex = new RegExp(BumpDownPattern, "i");
+    var AddRegex = new RegExp(AddPattern, "i");
+    var SubRegex = new RegExp(SubPattern, "i");
+
     return {
         startState: function () 
         {
@@ -18,11 +42,25 @@ CodeMirror.defineMode("hmrp", function ()
                 currentLine = currentLine + stream.next();
             }
 
-            if (currentLine.match(LabelRegex))
+            if (currentLine.match(LabelPattern))
             {
                 return 'property';
             }
-            if (currentLine.match(InstructionRegex))
+            
+            var isInstruction =
+                currentLine.match(InboxRegex) ||
+                currentLine.match(OutboxRegex) ||
+                currentLine.match(JumpZeroRegex) ||
+                currentLine.match(JumpNonZeroRegex) ||
+                currentLine.match(JumpRegex) ||
+                currentLine.match(CopyToRegex) ||
+                currentLine.match(CopyFromRegex) ||
+                currentLine.match(BumpUpRegex) ||
+                currentLine.match(BumpDownRegex) ||
+                currentLine.match(AddRegex) ||
+                currentLine.match(SubRegex);
+
+            if (isInstruction)
             {
                 return '';
             }
